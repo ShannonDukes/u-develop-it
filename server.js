a883fd4f3595ea6b6d940788b2e2d55cb2d429cb
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-09-04 19:04:15 -0700

    changes to db and server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const mysql = require('mysql2');
 const express = require('express');
-
+const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
@@ -17,7 +16,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '5447',
+    password: '',
     database: 'election'
   },
   console.log('Connected to the election database.')
@@ -39,7 +38,7 @@ app.get('/api/candidates', (req, res) => {
   });
 });
 
-// GET a single candidate
+// Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
   const sql = `SELECT * FROM candidates WHERE id = ?`;
   const params = [req.params.id];
@@ -80,13 +79,19 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
+
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  VALUES (?,?,?)`;
+    VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
 
   db.query(sql, params, (err, result) => {
@@ -100,8 +105,6 @@ app.post('/api/candidate', ({ body }, res) => {
     });
   });
 });
-
-
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
